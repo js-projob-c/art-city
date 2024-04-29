8d644c2f8096c77a44f0ebcea23ffa594f0842f4
Jasper <js.programming.c@gmail.com>
2024-04-30 03:00:48 +0800

    map get user response

--- server/src/features/user/dto/get-user-response.dto.ts
+++ server/src/features/user/dto/get-user-response.dto.ts
@@ -1,6 +1,16 @@
-import { Exclude } from 'class-transformer';
+import { Exclude, Type } from 'class-transformer';
+import { UserDetailEntity } from 'src/entities';
+
+export class GetUserResponseDetailDto implements Partial<UserDetailEntity> {
+  @Exclude()
+  deletedAt: string;
+}
 
 export class GetUserResponseDto {
   @Exclude()
   password: string;
+  @Type(() => GetUserResponseDetailDto)
+  detail: GetUserResponseDetailDto;
+  @Exclude()
+  deletedAt: string;
 }
