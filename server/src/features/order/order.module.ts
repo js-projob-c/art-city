7b65685069d0131570a51ea5e76275ee07dc7772
Jasper <js.programming.c@gmail.com>
2024-05-17 01:11:42 +0800

    order import

--- server/src/features/order/order.module.ts
+++ server/src/features/order/order.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 
+import { ExternalPartyModule } from '../external-party/external-party.module';
 import { OrderController } from './order.controller';
 import { OrderService } from './order.service';
 
 @Module({
+  imports: [ExternalPartyModule],
   controllers: [OrderController],
   providers: [OrderService],
 })
