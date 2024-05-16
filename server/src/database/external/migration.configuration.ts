13690df426f00812fe71c5bfe09f1a7fbf7ee57c
Jasper <js.programming.c@gmail.com>
2024-05-16 22:54:00 +0800

    move database related files

--- server/src/database/external/migration.configuration.ts
+++ server/src/database/external/migration.configuration.ts
@@ -2,7 +2,7 @@ import * as dotenv from 'dotenv';
 import * as path from 'path';
 import { DataSource } from 'typeorm';
 
-import * as Entities from '../../database/entities';
+import * as Entities from '../entities';
 
 const entities = Object.values(Entities);
 
