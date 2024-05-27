10e9a8a12b3be87bff56a6a28ab27b43f4bc1e64
Jasper <js.programming.c@gmail.com>
2024-05-28 03:11:24 +0800

    scheduler

---
+++ webapp/src/components/Scheduler/index.tsx
@@ -0,0 +1,102 @@
+"use client";
+import "react-calendar-timeline/lib/Timeline.css";
+
+import React from "react";
+import Timeline, {
+  CustomHeader,
+  DateHeader,
+  ReactCalendarTimelineProps,
+  SidebarHeader,
+  TimelineGroupBase,
+  TimelineHeaders,
+  TimelineItemBase,
+  Unit,
+} from "react-calendar-timeline";
+
+interface IProps {
+  unit?: Unit;
+  onUnitChange?: (unit: Unit) => void;
+}
+
+const Scheduler: React.FC<
+  ReactCalendarTimelineProps<TimelineItemBase<any>, TimelineGroupBase> & IProps
+> = (props) => {
+  const { unit: defaultUnit, onUnitChange, ...rest } = props;
+
+  const [unit, setUnit] = React.useState<Unit>(defaultUnit ?? "month");
+
+  return (
+    <div>
+      {/* <Timeline {...rest} /> */}
+
+      <Timeline
+        onBoundsChange={(ctx) => {
+          console.log("ctx", ctx);
+        }}
+        onZoom={(ctx, unit) => {
+          console.log("unit", unit);
+          setUnit(unit);
+          onUnitChange && onUnitChange(unit);
+        }}
+        {...rest}
+      >
+        <TimelineHeaders>
+          {/* <SidebarHeader>
+            {({ getRootProps }) => {
+              return <div {...getRootProps()}>Left</div>;
+            }}
+          </SidebarHeader> */}
+          <DateHeader unit="primaryHeader" />
+          {unit === "day" && (
+            <CustomHeader height={50} headerData={{ unit: "weekday" }}>
+              {({
+                headerContext: { intervals },
+                getRootProps,
+                getIntervalProps,
+                showPeriod,
+                data,
+              }: any) => {
+                return (
+                  <div {...getRootProps()}>
+                    {intervals.map((interval: any, i: number) => {
+                      const intervalStyle = {
+                        // lineHeight: "30px",
+                        textAlign: "center",
+                        borderLeft: "1px solid black",
+                        // cursor: "pointer",
+                        // backgroundColor: "Turquoise",
+                        color: "white",
+                      };
+                      return (
+                        <div
+                          key={i}
+                          onClick={() => {
+                            // showPeriod(interval.startTime, interval.endTime);
+                          }}
+                          {...getIntervalProps({
+                            interval,
+                            style: intervalStyle,
+                          })}
+                        >
+                          <div className="sticky">
+                            {interval.startTime
+                              .format("dddd")
+                              .substring(0, 3)
+                              .toUpperCase()}
+                          </div>
+                        </div>
+                      );
+                    })}
+                  </div>
+                );
+              }}
+            </CustomHeader>
+          )}
+          <DateHeader />
+        </TimelineHeaders>
+      </Timeline>
+    </div>
+  );
+};
+
+export default Scheduler;
